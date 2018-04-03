b6acb997ae08d793422f5f03f19e1ee3f10f75d1
rohit <rohitk8126@gmail.com>
2018-04-03 22:42:51 +0530

    code refactoring

--- spy_details.py
+++ spy_details.py
@@ -1,5 +1,17 @@
-spy={
-       'name': 'Mr. Rohit ',
-       'age': 20,
-       'rating': 2.2
-     }
+from datetime import datetime
+class Spy:
+    def __init__(self,name,salutation,age,rating):
+        self.name=salutation +" " +name
+        self.age=age
+        self.rating=rating
+        self.is_online=True
+        self.chats=[]
+        self.current_status_message=None
+
+spy=Spy('Rohit kumar','Mr.',19,1.1)
+
+class ChatMessage:
+    def __init__(self,message,sent_by_me):
+        self.message=message
+        self.time=datetime.now()
+        self.sent_by_me=sent_by_me
